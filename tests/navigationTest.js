37a6e4aaa591073f38013521c5f155f9c21bda26
surendarm2209 <surendarm2209@gmail.com>
2025-03-07 18:41:24 +0530

    Update navigationTest.js

--- tests/navigationTest.js
+++ tests/navigationTest.js
@@ -1,5 +1,6 @@
 const { driver, By, until, takeScreenshot } = require("./setup");
 const assert = require("assert");
+const driver = require("../setup");
 
 describe("Navigation Test Suite", function () {
   this.timeout(20000);
