74fdfe66a1587b24a2aeb75a3da5117a1663d1df
surendarm2209 <surendarm2209@gmail.com>
2025-03-07 21:48:26 +0530

    Update testSetup.js

--- tests/testSetup.js
+++ tests/testSetup.js
@@ -1,17 +1,34 @@
-const { getDriver } = require("./setup");
+import { getDriver, By, until, takeScreenshot } from "./setup.js";
+import { describe, it, before, after } from "mocha";
 
 let driver;
 
-before(async function () {
-  this.timeout(30000);
-  driver = await getDriver();
-});
+describe("Login Test", function () {
+  this.timeout(20000);
 
-after(async function () {
-  if (driver) {
+  before(async function () {
+    driver = await getDriver();
+  });
+
+  it("should open the login page", async function () {
+    await driver.get("https://www.saucedemo.com/");
+    await takeScreenshot(driver, "login_page_loaded");
+  });
+
+  it("should enter credentials and login", async function () {
+    let username = await driver.findElement(By.id("user-name"));
+    let password = await driver.findElement(By.id("password"));
+    let loginButton = await driver.findElement(By.id("login-button"));
+
+    await username.sendKeys("standard_user");
+    await password.sendKeys("secret_sauce");
+    await loginButton.click();
+
+    await driver.wait(until.elementLocated(By.className("inventory_list")), 5000);
+    await takeScreenshot(driver, "login_success");
+  });
+
+  after(async function () {
     await driver.quit();
-  }
+  });
 });
-
-module.exports = { driver };
-
